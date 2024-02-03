6bf32117e6b0ff7e6d103dddf00027d9480581bd
Yash Tyagi <contactyashtyagi@gmail.com>
2024-02-03 17:59:38 +0530

    basic http server using express

--- week 2/node-starter/http-server/index.js
+++ week 2/node-starter/http-server/index.js
@@ -2,10 +2,62 @@ const express = require("express");
 const port = 3000;
 const app = express();
 
-console.log(app);
+const user = [{
+    name : "Yash",
+    kidney: [{
+        healty: true
+    },{
+        healty: false
+    }]
+}]
 
 app.get('/',(req,res)=>{
-    res.send("Hello buddy");
+    res.json(user);
+})
+
+// Square function
+function square(num){
+    return num*num;
+}
+
+app.get('/square',(req,res)=>{
+    const num = req.query.n;
+    const result = square(num)
+    if(result){
+        res.send(`The square of ${num} is ${result}`);
+    }
+    else{
+        res.send(`Idiot, do you think ${num} is a number? huh?`);
+    }
+})
+
+// factorial function
+function factorial(num){
+    if (num == 1) {
+        return 1;
+    }
+    else{
+        return num*factorial(num-1);
+    }
+}
+
+app.get('/factorial',(req, res)=>{
+    const num = req.query.n;
+    const result = factorial(num)
+    if(result){
+        res.send(`The factorial of ${num} is ${result}`);
+    }
+    else{
+        res.send(`Idiot, do you think ${num} is a number? huh?`);
+    }
+})
+
+app.get('/king',(req,res)=>{
+    res.json({
+        "name" : "Yash",
+        "role" : "king",
+        // "req.header" : req.headers,
+    })
 })
 
 app.listen(port,()=>{
