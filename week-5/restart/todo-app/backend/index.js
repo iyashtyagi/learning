041a19dca7bf1008fa1891b753454d08edaa49b8
Yash Tyagi <contactyashtyagi@gmail.com>
2024-07-08 22:55:45 +0530

    added cors

--- week-5/restart/todo-app/backend/index.js
+++ week-5/restart/todo-app/backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv').config();
+const cors = require("cors");
 const {inputDataValidator, inputIdValidator} = require("./dataTypesValidator");
 const { todo } = require("./db");
 
@@ -8,6 +9,7 @@ const port = 3000;
 const dbConnectionString = process.env.DB_CONNECTION_STRING;
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 mongoose.connect(dbConnectionString);
 
