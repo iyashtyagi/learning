7032c1242ad153ad791e44fc3c08ad89e8c352f5
Yash Tyagi <contactyashtyagi@gmail.com>
2024-07-13 16:21:25 +0530

    minimized re-rendring

--- week-6/experiment/src/components/NameButton.jsx
+++ week-6/experiment/src/components/NameButton.jsx
@@ -1,13 +1,19 @@
-export function NameButton(props){
+import { Header } from "./Header";
+import { useState } from "react";
+
+export function NameButton(){
+
+    const [name, setName] = useState("Yash Tyagi");
     
     function buttonHandler(){
         const randomNum = Math.random();
-        props.setName(randomNum);
+        setName(randomNum);
     }
 
     return (
         <>
             <button onClick={buttonHandler}>Click me to change the name</button>
+            <Header name = {name}/>
         </>
     )
 }
