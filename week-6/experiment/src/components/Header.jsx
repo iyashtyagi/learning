56b8f94d9f37ca165e93743b885437790baf5794
Yash Tyagi <contactyashtyagi@gmail.com>
2024-07-13 16:10:05 +0530

    week-6 started and created custom name header component

---
+++ week-6/experiment/src/components/Header.jsx
@@ -0,0 +1,10 @@
+
+export function Header(props){
+    
+    return(
+        <div>
+            <h1>My name is { props.name} </h1>
+        </div>
+    )
+
+}
---
+++ week-6/experiment/src/components/NameButton.jsx
@@ -0,0 +1,13 @@
+export function NameButton(props){
+    
+    function buttonHandler(){
+        const randomNum = Math.random();
+        props.setName(randomNum);
+    }
+
+    return (
+        <>
+            <button onClick={buttonHandler}>Click me to change the name</button>
+        </>
+    )
+}
