9cff157329a937803afb7585007d053103009de8
Yash Tyagi <contactyashtyagi@gmail.com>
2024-07-14 15:50:34 +0530

    learned about useEffect hook

---
+++ week-6/use-effect/src/App.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react"
+
+export default function App(){
+    const [todos, setTodos] = useState([]);
+
+    return(
+        <>
+            <Todo todos = {todos} setTodos = {setTodos} />
+        </>
+    )
+}
+
+
+function Todo({todos, setTodos}){
+    console.log("here we are",todos);
+
+    useEffect(()=>{
+        fetch("https://sum-server.100xdevs.com/todos")
+            .then(async(res)=>{
+                const json = await res.json();
+                setTodos(json.todos);
+            })
+    },[])
+
+    return(
+        <div>
+            {todos.map((todo)=>{
+                return <div key={todo.id}>
+                    <p><b>Title: </b>{todo.title}</p>
+                    <p><b>Description: </b>{todo.description}</p>
+                    <button> {todo.completed?"Completed":"Mark as completed"} </button>
+                </div>
+            })}
+        </div>
+    )
+}
