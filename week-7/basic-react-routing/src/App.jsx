900dfa952169f7c0c825a7ada5ff85584b3e56c0
Yash Tyagi <contactyashtyagi@gmail.com>
2024-07-16 23:46:03 +0530

    added navbar

--- week-7/basic-react-routing/src/App.jsx
+++ week-7/basic-react-routing/src/App.jsx
@@ -2,15 +2,19 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 import { Dashboard } from "./components/Dashboard";
 import { Landing } from "./components/Landing";
+import { NavBar } from './components/NavBar';
 
 export default function App(){
 
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/dashboard" element={<Dashboard/>} />
-                <Route path="/" element = {<Landing/>} />
-            </Routes>
-        </BrowserRouter>
+        <>
+            <NavBar></NavBar>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/dashboard" element={<Dashboard/>} />
+                    <Route path="/" element = {<Landing/>} />
+                </Routes>
+            </BrowserRouter>
+        </>
     )
 }
---
+++ week-7/basic-react-routing/src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+export function NavBar(){
+
+    return(
+        <div style={{color:"gold",
+            background : "black",
+            fontSize : "100px"
+        }}>
+            This is a Navbar.
+        </div>
+    )
+}
