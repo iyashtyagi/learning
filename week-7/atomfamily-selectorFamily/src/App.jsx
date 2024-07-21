58d49785b6d3daa640652125b4f3fadf08aa3f71
Yash Tyagi <contactyashtyagi@gmail.com>
2024-07-21 17:41:11 +0530

    learned about atomFamily in recoil

---
+++ week-7/atomfamily-selectorFamily/src/App.jsx
@@ -0,0 +1,30 @@
+import {RecoilRoot, useRecoilValue} from 'recoil'
+import { todoAtomsFamily } from './atoms'
+
+export default function App(){ 
+
+    return(<>
+
+        <RecoilRoot>
+            <Todo id={1}/>
+            <Todo id={2}/>
+            <Todo id={3}/>
+            <Todo id={4}/>
+            <Todo id={5}/>
+            <Todo id={6}/>
+        </RecoilRoot>
+        
+    </>)
+}
+
+function Todo({id}){
+    const currentTodo = useRecoilValue(todoAtomsFamily(id));
+
+    return(
+        <div>
+            {currentTodo.id}
+            <p><b>Title: </b>{currentTodo.title}</p>
+            <p><b>Description: </b>{currentTodo.description}</p>
+        </div>
+    )
+}
---
+++ week-7/atomfamily-selectorFamily/src/todos.js
@@ -0,0 +1,12 @@
+export const TODOS = [
+    { "id": 1, "title": "Buy groceries", "description": "Milk, bread, eggs, and fruits", "completed": false },
+    { "id": 2, "title": "Finish report", "description": "Complete the monthly sales report", "completed": false },
+    { "id": 3, "title": "Call mom", "description": "Catch up with mom and discuss weekend plans", "completed": true },
+    { "id": 4, "title": "Exercise", "description": "Go for a 30-minute run in the park", "completed": false },
+    { "id": 5, "title": "Read a book", "description": "Read at least 20 pages of 'The Great Gatsby'", "completed": true },
+    { "id": 6, "title": "Clean the house", "description": "Vacuum, dust, and mop all rooms", "completed": false },
+    { "id": 7, "title": "Water plants", "description": "Water all indoor and outdoor plants", "completed": true },
+    { "id": 8, "title": "Prepare dinner", "description": "Cook spaghetti with tomato sauce", "completed": false },
+    { "id": 9, "title": "Study for exam", "description": "Review chapters 4, 5, and 6 for the biology exam", "completed": false },
+    { "id": 10, "title": "Attend meeting", "description": "Join the Zoom meeting at 3 PM with the project team", "completed": true }
+];
