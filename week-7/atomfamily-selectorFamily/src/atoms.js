e4e6a322b750e263613c78476ff548c7b094fe2d
Yash Tyagi <contactyashtyagi@gmail.com>
2024-07-21 19:02:06 +0530

    learned about selectorFamily and optional chaining "?"

--- week-7/atomfamily-selectorFamily/src/atoms.js
+++ week-7/atomfamily-selectorFamily/src/atoms.js
@@ -1,15 +1,29 @@
-import { atomFamily } from 'recoil'
+import { atomFamily, selectorFamily } from 'recoil'
 import { TODOS } from './todos';
+import axios from 'axios'
 
+// atomFamily
+// export const todoAtomsFamily = atomFamily({
+//     key : "todoAtomsFamily",
+//     default : id => {
+//         let foundTodo = null;
+//         for (let i = 0; i < TODOS.length; i++) {
+//             if (TODOS[i].id === id) {
+//                 foundTodo = TODOS[i];   
+//             }
+//         }
+//         return foundTodo;
+//     }
+// })
+
+// selectorFamily
 export const todoAtomsFamily = atomFamily({
     key : "todoAtomsFamily",
-    default : id => {
-        let foundTodo = null;
-        for (let i = 0; i < TODOS.length; i++) {
-            if (TODOS[i].id === id) {
-                foundTodo = TODOS[i];   
-            }
+    default: selectorFamily({
+        key : "todoSelectorFamily",
+        get : (id) => async({get}) => {
+            let todo = (await axios.get(`https://sum-server.100xdevs.com/todo?id=${id}`)).data;
+            return todo;
         }
-        return foundTodo;
-    }
+    })
 })
