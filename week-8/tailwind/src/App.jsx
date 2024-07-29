0d912ce6192676710e03efa9acb4e2bb8ef22406
Yash Tyagi <contactyashtyagi@gmail.com>
2024-07-29 22:59:57 +0530

    tailwind inti

---
+++ week-8/tailwind/src/App.jsx
@@ -0,0 +1,16 @@
+export default function App(){
+
+    return(
+        <div style={{display:"flex" }}>
+            <div style={{background:""}}>
+                This is div 1
+            </div>
+            <div>
+                This is div 2
+            </div>
+            <div>
+                This is div 3
+            </div>
+        </div>
+    )
+}
---
+++ week-8/tailwind/src/main.jsx
@@ -0,0 +1,6 @@
+import ReactRoot from "react-dom/client"
+import App from "./App.jsx"
+
+ReactRoot.createRoot(document.getElementById("root")).render(
+    <App/>
+)
