c4499aafdfd2f2d1a3568e032865fd1187daed78
Yash Tyagi <contactyashtyagi@gmail.com>
2024-07-30 23:21:39 +0530

    tailwind basic flex and grid

--- week-8/tailwind/src/App.jsx
+++ week-8/tailwind/src/App.jsx
@@ -1,16 +1,39 @@
 export default function App(){
 
     return(
-        <div style={{display:"flex" }}>
-            <div style={{background:""}}>
+        <>
+        {/* flex */}
+        <div className ='flex justify-center'> 
+            <div className="bg-black text-white">
                 This is div 1
             </div>
-            <div>
+            <div className="bg-blue-500 text-white">
                 This is div 2
             </div>
-            <div>
-                This is div 3
+            <div className="bg-yellow-300">
+               This is div 3
             </div>
         </div>
+        <br></br>
+
+
+        {/* Grid */}
+        <div className ='grid grid-cols-10'> 
+            <div className="bg-black text-white">
+                This is div 1
+            </div>
+            <div className="bg-blue-500 text-white col-span-4">
+                This is div 2
+            </div>
+            <div className="bg-yellow-300">
+               This is div 3
+            </div>
+            <div className="bg-pink-300">
+               This is div 4
+            </div>
+        </div>
+
+        </>
+        
     )
-}
+}  
--- week-8/tailwind/src/main.jsx
+++ week-8/tailwind/src/main.jsx
@@ -1,5 +1,6 @@
 import ReactRoot from "react-dom/client"
 import App from "./App.jsx"
+import "./index.css"
 
 ReactRoot.createRoot(document.getElementById("root")).render(
     <App/>
