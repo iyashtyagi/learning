c4801cfe5914596333e6972e4b5a4aaf623f73e7
Yash Tyagi <contactyashtyagi@gmail.com>
2024-07-31 23:12:26 +0530

    added AmountProcessedCard

--- week-8/dukan-components/src/App.jsx
+++ week-8/dukan-components/src/App.jsx
@@ -1,10 +1,12 @@
 import { AmountPendingCard } from "./components/AmountPendingCard";
+import { AmountProcessedCard } from "./components/AmountProcessedCard";
 
 export default function App(){
     return(
         <div className="">
             <div  className="grid grid-cols-4">
                 <AmountPendingCard amount={"92,312.20"} orders={13}/>
+                <AmountProcessedCard amount={"23,92,312.19"} />
             </div>
         </div>
     )
