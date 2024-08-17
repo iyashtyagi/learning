3e8a0fdc4e49c35a1387168cd6cd4452352b186d
Yash Tyagi <contactyashtyagi@gmail.com>
2024-08-17 17:26:43 +0530

    added account model and update user model

--- week-8/payment-wallet/backend/db.js
+++ week-8/payment-wallet/backend/db.js
@@ -2,13 +2,42 @@ const mongoose = require("mongoose");
 const { DB_CONNECTION_STRING } = require("./config");
 
 const userSchema = mongoose.Schema({
-    email : String,
-    firstName : String,
-    lastName : String,
-    password : String
+    email : {
+        type : String,
+        unique : true,
+        trim : true,
+        require : true
+    },
+    firstName : {
+        type : String,
+        trim : true,
+        require : true
+    },
+    lastName :  {
+        type : String,
+        trim : true,
+        require : true
+    },
+    password :  {
+        type : String,
+        require : true
+    }
+})
+
+const accountSchema = mongoose.Schema({
+    userId : {
+        type : mongoose.Schema.Types.ObjectId, //reference to User model
+        ref : 'User',
+        require : true
+    },
+    balance : {
+        type : Number,
+        require : true
+    }
 })
 
 const User = mongoose.model("user", userSchema);
+const Account = mongoose.model("account", accountSchema);
 
 const dbConnect = async () =>{
     const dbUrl = DB_CONNECTION_STRING;
@@ -18,5 +47,6 @@ const dbConnect = async () =>{
 
 module.exports ={
     User,
+    Account,
     dbConnect
 }
