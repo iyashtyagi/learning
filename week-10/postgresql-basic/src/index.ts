d191bd77dd3524e8d9b692484eeeaddddcd97ce1
Yash Tyagi <contactyashtyagi@gmail.com>
2024-09-27 01:36:59 +0530

    searching

--- week-10/postgresql-basic/src/index.ts
+++ week-10/postgresql-basic/src/index.ts
@@ -36,4 +36,24 @@ async function insertData() {
     await client.end(); 
 }
 
-insertData();
+async function getUser(email:string){
+    try{
+        const query = `SELECT * FROM users WHERE email = $1`;
+        const values = [email];
+        const result = await client.query(query, values);
+        if(result.rows.length > 0){
+            console.log('User found: ',result.rows[0]);
+            return result.rows[0];
+        }
+        else{
+            console.log("No user found");
+            return null;
+        }
+    }
+    catch(err){
+        console.error(err);
+        return null;
+    }
+}
+
+getUser("yash2@gmail.com");
